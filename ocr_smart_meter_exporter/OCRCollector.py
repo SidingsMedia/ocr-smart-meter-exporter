ae67b5744391b29e347218d49826771e5027b9d5
Matthew Nickson <mnickson@sidingsmedia.com>
2022-06-16 20:59:26 +0100

    Added more prometheus metrics
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- ocr_smart_meter_exporter/OCRCollector.py
+++ ocr_smart_meter_exporter/OCRCollector.py
@@ -6,7 +6,7 @@ from prometheus_client.core import GaugeMetricFamily
 from .Logging import log
 from .ocr import OCR
  
- 
+
 class OCRCollector(object):
 
     def __init__(self, ocr_processor: OCR) -> None:
@@ -29,7 +29,23 @@ class OCRCollector(object):
 
         log.debug("OCRCollector", "Call made to exporter")
 
-        current_usage = GaugeMetricFamily("current_usage", "The current usage as read from the camera")
-        current_usage.add_metric([], self._ocr.get())
+        value = self._ocr.get()
+
+        current_usage = GaugeMetricFamily("electricity_current_usage", "The current usage as read from the camera")
+        current_usage.add_metric([], abs(value))
+
+        current_import = GaugeMetricFamily("electricity_current_import", "The current import as read from the camera")
+        if value >= 0:
+            current_import.add_metric([], value)
+        else:
+            current_import.add_metric([], 0)
+        
+        current_export = GaugeMetricFamily("electricity_current_export", "The current export to the grid as read from the camera")
+        if value <= 0:
+            current_export.add_metric([], abs(value))
+        else:
+            current_export.add_metric([], 0)
 
         yield current_usage
+        yield current_import
+        yield current_export
